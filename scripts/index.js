4ce1c8f44517aab8b85d023abfda79144a23b502
Alzir <alzirfoto@gmail.com>
2022-09-28 13:21:41 -0300

    commit07

--- scripts/index.js
+++ scripts/index.js
@@ -8,72 +8,72 @@ function checkWinner() {
     elements[1].innerHTML === elements[2].innerHTML &&
     elements[2].innerHTML.length !== 0
   ) {
-    elements[0].style = "background-color: yellow;";
-    elements[1].style = "background-color: yellow;";
-    elements[2].style = "background-color: yellow;";
+    elements[0].style = "filter: grayscale(100%)";
+    elements[1].style = "filter: grayscale(100%)";
+    elements[2].style = "filter: grayscale(100%)";
     return true;
   } else if (
     elements[3].innerHTML === elements[4].innerHTML &&
     elements[4].innerHTML === elements[5].innerHTML &&
     elements[5].innerHTML.length !== 0
   ) {
-    elements[3].style = "background-color: yellow;";
-    elements[4].style = "background-color: yellow;";
-    elements[5].style = "background-color: yellow;";
+    elements[3].style = "filter: grayscale(100%)";
+    elements[4].style = "filter: grayscale(100%)";
+    elements[5].style = "filter: grayscale(100%)";
     return true;
   } else if (
     elements[6].innerHTML === elements[7].innerHTML &&
     elements[7].innerHTML === elements[8].innerHTML &&
     elements[8].innerHTML.length !== 0
   ) {
-    elements[6].style = "background-color: yellow;";
-    elements[7].style = "background-color: yellow;";
-    elements[8].style = "background-color: yellow;";
+    elements[6].style = "filter: grayscale(100%)";
+    elements[7].style = "filter: grayscale(100%)";
+    elements[8].style = "filter: grayscale(100%)";
     return true;
   } else if (
     elements[0].innerHTML === elements[3].innerHTML &&
     elements[3].innerHTML === elements[6].innerHTML &&
     elements[6].innerHTML.length !== 0
   ) {
-    elements[0].style = "background-color: yellow;";
-    elements[3].style = "background-color: yellow;";
-    elements[6].style = "background-color: yellow;";
+    elements[0].style = "filter: grayscale(100%)";
+    elements[3].style = "filter: grayscale(100%)";
+    elements[6].style = "filter: grayscale(100%)";
     return true;
   } else if (
     elements[1].innerHTML === elements[4].innerHTML &&
     elements[4].innerHTML === elements[7].innerHTML &&
     elements[7].innerHTML.length !== 0
   ) {
-    elements[1].style = "background-color: yellow;";
-    elements[4].style = "background-color: yellow;";
-    elements[7].style = "background-color: yellow;";
+    elements[1].style = "filter: grayscale(100%)";
+    elements[4].style = "filter: grayscale(100%)";
+    elements[7].style = "filter: grayscale(100%)";
     return true;
   } else if (
     elements[2].innerHTML === elements[5].innerHTML &&
     elements[5].innerHTML === elements[8].innerHTML &&
     elements[8].innerHTML.length !== 0
   ) {
-    elements[2].style = "background-color: yellow;";
-    elements[5].style = "background-color: yellow;";
-    elements[8].style = "background-color: yellow;";
+    elements[2].style = "filter: grayscale(100%)";
+    elements[5].style = "filter: grayscale(100%)";
+    elements[8].style = "filter: grayscale(100%)";
     return true;
   } else if (
     elements[0].innerHTML === elements[4].innerHTML &&
     elements[4].innerHTML === elements[8].innerHTML &&
     elements[8].innerHTML.length !== 0
   ) {
-    elements[0].style = "background-color: yellow;";
-    elements[4].style = "background-color: yellow;";
-    elements[8].style = "background-color: yellow;";
+    elements[0].style = "filter: grayscale(100%);";
+    elements[4].style = "filter: grayscale(100%);";
+    elements[8].style = "filter: grayscale(100%);";
     return true;
   } else if (
     elements[2].innerHTML === elements[4].innerHTML &&
     elements[4].innerHTML === elements[6].innerHTML &&
     elements[6].innerHTML.length !== 0
   ) {
-    elements[2].style = "background-color: yellow;";
-    elements[4].style = "background-color: yellow;";
-    elements[6].style = "background-color: yellow;";
+    elements[2].style = "filter: grayscale(100%);";
+    elements[4].style = "filter: grayscale(100%);";
+    elements[6].style = "filter: grayscale(100%);";
     return true;
   }
   return false;
