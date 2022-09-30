da411a775c01e07c9d50805772e9015409f92dde
Alzir <alzirfoto@gmail.com>
2022-09-30 16:11:43 -0300

    commitgameover

--- scripts/index.js
+++ scripts/index.js
@@ -8,83 +8,93 @@ function checkWinner() {
     elements[1].innerHTML === elements[2].innerHTML &&
     elements[2].innerHTML.length !== 0
   ) {
-    elements[0].style = "filter: grayscale(100%)";
-    elements[1].style = "filter: grayscale(100%)";
-    elements[2].style = "filter: grayscale(100%)";
+    elements[0].style = "filter: contrast(150%)";
+    elements[1].style = "filter: contrast(150%)";
+    elements[2].style = "filter: contrast(150%)";
     return true;
   } else if (
     elements[3].innerHTML === elements[4].innerHTML &&
     elements[4].innerHTML === elements[5].innerHTML &&
     elements[5].innerHTML.length !== 0
   ) {
-    elements[3].style = "filter: grayscale(100%)";
-    elements[4].style = "filter: grayscale(100%)";
-    elements[5].style = "filter: grayscale(100%)";
+    elements[3].style = "filter: contrast(150%)";
+    elements[4].style = "filter: contrast(150%)";
+    elements[5].style = "filter: contrast(150%)";
     return true;
   } else if (
     elements[6].innerHTML === elements[7].innerHTML &&
     elements[7].innerHTML === elements[8].innerHTML &&
     elements[8].innerHTML.length !== 0
   ) {
-    elements[6].style = "filter: grayscale(100%)";
-    elements[7].style = "filter: grayscale(100%)";
-    elements[8].style = "filter: grayscale(100%)";
+    elements[6].style = "filter: contrast(150%)";
+    elements[7].style = "filter: contrast(150%)";
+    elements[8].style = "filter: contrast(150%)";
     return true;
   } else if (
     elements[0].innerHTML === elements[3].innerHTML &&
     elements[3].innerHTML === elements[6].innerHTML &&
     elements[6].innerHTML.length !== 0
   ) {
-    elements[0].style = "filter: grayscale(100%)";
-    elements[3].style = "filter: grayscale(100%)";
-    elements[6].style = "filter: grayscale(100%)";
+    elements[0].style = "filter: contrast(150%)";
+    elements[3].style = "filter: contrast(150%)";
+    elements[6].style = "filter: contrast(150%)";
     return true;
   } else if (
     elements[1].innerHTML === elements[4].innerHTML &&
     elements[4].innerHTML === elements[7].innerHTML &&
     elements[7].innerHTML.length !== 0
   ) {
-    elements[1].style = "filter: grayscale(100%)";
-    elements[4].style = "filter: grayscale(100%)";
-    elements[7].style = "filter: grayscale(100%)";
+    elements[1].style = "filter: contrast(150%)";
+    elements[4].style = "filter: contrast(150%)";
+    elements[7].style = "filter: contrast(150%)";
     return true;
   } else if (
     elements[2].innerHTML === elements[5].innerHTML &&
     elements[5].innerHTML === elements[8].innerHTML &&
     elements[8].innerHTML.length !== 0
   ) {
-    elements[2].style = "filter: grayscale(100%)";
-    elements[5].style = "filter: grayscale(100%)";
-    elements[8].style = "filter: grayscale(100%)";
+    elements[2].style = "filter: contrast(150%)";
+    elements[5].style = "filter: contrast(150%)";
+    elements[8].style = "filter: contrast(150%)";
     return true;
   } else if (
     elements[0].innerHTML === elements[4].innerHTML &&
     elements[4].innerHTML === elements[8].innerHTML &&
     elements[8].innerHTML.length !== 0
   ) {
-    elements[0].style = "filter: grayscale(100%);";
-    elements[4].style = "filter: grayscale(100%);";
-    elements[8].style = "filter: grayscale(100%);";
+    elements[0].style = "filter: contrast(150%)";
+    elements[4].style = "filter: contrast(150%)";
+    elements[8].style = "filter: contrast(150%)";
     return true;
   } else if (
     elements[2].innerHTML === elements[4].innerHTML &&
     elements[4].innerHTML === elements[6].innerHTML &&
     elements[6].innerHTML.length !== 0
   ) {
-    elements[2].style = "filter: grayscale(100%);";
-    elements[4].style = "filter: grayscale(100%);";
-    elements[6].style = "filter: grayscale(100%);";
+    elements[2].style = "filter: contrast(150%)";
+    elements[4].style = "filter: contrast(150%)";
+    elements[6].style = "filter: contrast(150%)";
     return true;
   }
   return false;
 }
 
+function checkGameOver() {
+  let elements = document.getElementById("container").children;
+  let gameOver = true;
+  for(let indice = 0; indice < elements.length; indice++) {
+    if(elements[indice].innerHTML.length === 0) {
+      gameOver = false;
+    }
+  }
+  return gameOver;
+}
 
 let winner = false;
-
+let gameOver = false;
 
 function mark(element) {
-  if (!winner) {
+  if (!winner && !gameOver) {
     if (element.innerHTML.length === 0) {
       element.innerHTML = jogador;
 
@@ -94,7 +104,48 @@ function mark(element) {
         document.getElementById("containerMsgs").style = "background-color: yellow";
         document.getElementById("messages").style = "padding: 60px";
         document.getElementById("button").style.visibility = "visible";
+      } else {
+        
 
+        if (!winner)
+        document.getElementById("messages").innerText = "Ainda não há ganhadores!";
+        document.getElementById("messages").style = "padding: 60px";
+      document.getElementById("button").style.visibility = "visible";
+
+
+
+        gameOver = checkGameOver();
+
+        
+        if(gameOver) {
+          document.getElementById("messages").innerText = "Fim de jogo!";
+
+ 
+          
+          document.getElementById("c0").innerHTML = '';
+          document.getElementById("c0").style = "background-color: black";
+          document.getElementById("c1").innerHTML = '';
+          document.getElementById("c1").style = "background-color: black";
+          document.getElementById("c2").innerHTML = '';
+          document.getElementById("c2").style = "background-color: black";
+          document.getElementById("c3").innerHTML = '';
+          document.getElementById("c3").style = "background-color: black";
+          document.getElementById("c4").innerHTML = '';
+          document.getElementById("c4").style = "background-color: black";
+          document.getElementById("c5").innerHTML = '';
+          document.getElementById("c5").style = "background-color: black";
+          document.getElementById("c6").innerHTML = '';
+          document.getElementById("c6").style = "background-color: black";
+          document.getElementById("c7").innerHTML = '';
+          document.getElementById("c7").style = "background-color: black";
+          document.getElementById("c8").innerHTML = '';
+          document.getElementById("c8").style = "background-color: black";
+          document.getElementById("containerMsgs").style = "background-color: yellow";
+          document.getElementById("messages").style = "padding: 60px";
+          document.getElementById("button").style.visibility = "visible";
+          return;
+        }
+      
 
       }
       if (jogador === '<img src = "/images/lula.jpg">') {
@@ -103,10 +154,7 @@ function mark(element) {
         jogador = '<img src = "/images/lula.jpg">';
       }
 
-      if (!winner)
-        document.getElementById("messages").innerText = "Não há ganhadores!";
-        document.getElementById("messages").style = "padding: 60px";
-      document.getElementById("button").style.visibility = "visible";
+      
 
 
     } else {
